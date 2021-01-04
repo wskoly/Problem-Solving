094850e42170483ae64945eb341a7f36ec655d39
Wahid Sadique Koly <57163453+wskoly@users.noreply.github.com>
2021-01-04 21:12:34 +0600

    Create Grading Students.py

---
+++ Hacker Rank/Grading Students.py
@@ -0,0 +1,38 @@
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'gradingStudents' function below.
+#
+# The function is expected to return an INTEGER_ARRAY.
+# The function accepts INTEGER_ARRAY grades as parameter.
+#
+
+def gradingStudents(grades):
+    k=0
+    for i in grades:
+        if i%5>=3 and i>=38:
+            grades[k]+=(5-i%5)
+        k+=1
+    return grades
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    grades_count = int(input().strip())
+
+    grades = []
+
+    for _ in range(grades_count):
+        grades_item = int(input().strip())
+        grades.append(grades_item)
+
+    result = gradingStudents(grades)
+
+    fptr.write('\n'.join(map(str, result)))
+    fptr.write('\n')
+
+    fptr.close()
